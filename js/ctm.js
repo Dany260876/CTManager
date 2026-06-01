7c9e999da1fa07021e91e57808fffa8e76f94012
Dany <dany.tami@gmail.com>
2026-06-01 11:22:34 +0200

    Add files via upload

--- js/ctm.js
+++ js/ctm.js
@@ -116,11 +116,11 @@ class CTContext {
             }
             else {
                 if (configuration) {
-                    this.duration = configuration.dailyDuration;
-                    this.rules = configuration.rules;
-                    this.timestamp = new Date();
-                    this.history = [];
-                    this.locked = false;
+                    me.duration = configuration.dailyDuration;
+                    me.rules = configuration.rules;
+                    me.timestamp = new Date();
+                    me.history = [];
+                    me.locked = false;
                 }
                 res.resolve();
             }
